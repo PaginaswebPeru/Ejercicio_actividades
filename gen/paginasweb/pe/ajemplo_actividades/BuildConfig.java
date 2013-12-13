3ea1fae7847af02d7bbfc59b5e1c9fe8629a47b7
Jorge Sandoval <jorge.sandoval@paginasweb.pe>
2013-12-13 15:41:16 -0500

    primer commit

---
+++ gen/paginasweb/pe/ajemplo_actividades/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package paginasweb.pe.ajemplo_actividades;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/paginasweb/pe/ajemplo_actividades/Segundo_Layout.java
@@ -0,0 +1,13 @@
+package paginasweb.pe.ajemplo_actividades;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Segundo_Layout extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.segundo_layout);
+	}
+}
